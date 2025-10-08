2928d51f5ac94ff1d0164e423caa451555d5af97
Alexander Kies <kies@fias.uni-frankfurt.de>
2025-10-08 15:18:32 +0200

    Create stacks.py

---
+++ src/esm_aarhus/pypsa/plotting/results/stacks.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from ..styles.base import set_defaults
+
+def generation_stack(df: pd.DataFrame, *, title="Generation stack", ax=None):
+    """
+    Plot a stacked area of generation by technology.
+
+    df: DataFrame indexed by time, columns are technologies (MW).
+    """
+    set_defaults()
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = ax.figure
+    df.plot.area(ax=ax)
+    ax.set_ylabel("MW")
+    ax.set_title(title)
+    return fig, ax
